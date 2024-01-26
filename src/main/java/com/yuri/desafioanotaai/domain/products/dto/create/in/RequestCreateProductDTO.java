1e8e936393386993a35bfd104dc44dad10c5a28b
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-01-26 14:04:13 -0300

    Adicionada validação nos DTO's de entrada da entidade Product

--- src/main/java/com/yuri/desafioanotaai/domain/products/dto/create/in/RequestCreateProductDTO.java
+++ src/main/java/com/yuri/desafioanotaai/domain/products/dto/create/in/RequestCreateProductDTO.java
@@ -4,16 +4,22 @@ package com.yuri.desafioanotaai.domain.products.dto.create.in;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yuri.desafioanotaai.domain.category.model.Category;
 import com.yuri.desafioanotaai.domain.products.model.Product;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RequestCreateProductDTO {
 
+    @NotBlank(message = "{errors.product.title.required}")
     private String title;
+    @NotBlank(message = "{errors.product.description.required}")
     private String description;
+    @NotBlank(message = "{errors.product.price.required}")
     private double price;
     @JsonProperty("owner")
+    @NotBlank(message = "{errors.product.owner.required}")
     private String ownerId;
+    @NotBlank(message = "{errors.product.category.required}")
     private String category;
 
     public Product toProduct() {
--- src/main/java/com/yuri/desafioanotaai/domain/products/dto/update/in/ResquestUpdateProductDTO.java
+++ src/main/java/com/yuri/desafioanotaai/domain/products/dto/update/in/ResquestUpdateProductDTO.java
@@ -2,14 +2,19 @@ package com.yuri.desafioanotaai.domain.products.dto.update.in;
 
 import com.yuri.desafioanotaai.domain.category.model.Category;
 import com.yuri.desafioanotaai.domain.products.model.Product;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ResquestUpdateProductDTO {
 
+    @NotBlank(message = "{errors.product.title.required}")
     private String title;
+    @NotBlank(message = "{errors.product.description.required}")
     private String description;
+    @NotBlank(message = "{errors.product.price.required}")
     private double price;
+    @NotBlank(message = "{errors.product.category.required}")
     private String category;
 
     public Product toProduct() {
