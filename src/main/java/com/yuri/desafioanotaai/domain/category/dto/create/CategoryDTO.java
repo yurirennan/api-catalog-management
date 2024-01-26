e1f1bcd8975e937a0f8d249d65d14ab0b2d0a871
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-01-26 10:39:48 -0300

    Adicionado annotations para verificação nos DTO's de Category

--- src/main/java/com/yuri/desafioanotaai/domain/category/dto/create/CategoryDTO.java
+++ src/main/java/com/yuri/desafioanotaai/domain/category/dto/create/CategoryDTO.java
@@ -2,14 +2,18 @@ package com.yuri.desafioanotaai.domain.category.dto.create;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.yuri.desafioanotaai.domain.category.model.Category;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CategoryDTO {
 
+    @NotBlank(message = "{errors.category.title.required}")
     private String title;
+    @NotBlank(message = "{errors.category.description.required}")
     private String description;
     @JsonProperty("owner")
+    @NotBlank(message = "{errors.category.owner.required}")
     private String ownerId;
 
     public Category toCategory() {
--- src/main/java/com/yuri/desafioanotaai/domain/category/dto/update/UpdateCategoryDTO.java
+++ src/main/java/com/yuri/desafioanotaai/domain/category/dto/update/UpdateCategoryDTO.java
@@ -1,11 +1,14 @@
 package com.yuri.desafioanotaai.domain.category.dto.update;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdateCategoryDTO {
 
+    @NotBlank(message = "{errors.category.title.required}")
     private String title;
+    @NotBlank(message = "{errors.category.description.required}")
     private String description;
 
 }
